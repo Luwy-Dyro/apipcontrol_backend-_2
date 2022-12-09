f441106f30a3eb996d792bc47438142b4b61bd2e
Luwy-Dyro <luwy.dyro@gmail.com>
2022-12-08 19:01:51 -0500

    first project

---
+++ src/main/java/com/apirest/sisgespro/ServletInitializer.java
@@ -0,0 +1,27 @@
+package com.apirest.sisgespro;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ApirestSisgesproApplication.class);
+	}
+
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("https://controlsbyprojects.azurewebsites.net").allowedMethods("*").allowedHeaders("*");
+			}
+		};
+	}
+	
+}
